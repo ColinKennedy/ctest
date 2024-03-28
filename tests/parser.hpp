e1fbafa282e2e35f3ecb6b8d47cbd724786dc5fc
Colin Kennedy <colinvfx@gmail.com>
2024-03-27 23:45:45 -0700

    Simplified the regex

--- tests/parser.hpp
+++ tests/parser.hpp
@@ -10,10 +10,37 @@ namespace parser {
 
 namespace details
 {
-    std::regex const TEST_REGEX {"TEST \\d+/\\d+ (\\w+):(\\w+)"};
+    // TODO: Remove the need for TEST_REGEX, later
+    std::regex const TEST_REGEX {"TEST \\d+/\\d+ (\\w+):(\\w+).*"};
     std::regex const RESULTS_REGEX {
         "RESULTS: (\\d+) tests \\((\\d+) ok, (\\d+) failed, (\\d+) skipped\\) ran in (\\d+\\.\\d+) ms"
     };
+
+    bool ends_with(std::string const &full, std::string const &end) {
+        if (full.length() < end.length())
+        {
+            return false;
+        }
+
+        return full.compare(full.length() - end.length(), end.length(), end) == 0;
+    }
+
+    bool starts_with(std::string const &full, std::string const &start) {
+        if (full.length() < start.length())
+        {
+            return false;
+        }
+
+        return full.compare(0, start.length(), start) == 0;
+    }
+
+    bool is_failed(std::string const &text) { return ends_with(text, "[FAILED]"); }
+
+    bool is_okay(std::string const &text) { return ends_with(text, "[OK]"); }
+
+    bool is_skipped(std::string const &text) { return ends_with(text, "[SKIPPED]"); }
+
+    bool is_testing_header(std::string const &text) { return starts_with(text, "TEST "); }
 }
 
 enum TestStatus
@@ -48,7 +75,6 @@ TestResults parse_std_out(std::string const text)
     std::vector<SingleTestCase> cases;
     TestResults output;
     std::string previous;
-    bool in_test {false};
 
     std::smatch matches;
 
@@ -73,47 +99,36 @@ TestResults parse_std_out(std::string const text)
 
         if (std::regex_search(buffer, matches, details::TEST_REGEX))
         {
-            in_test = true;
             suite_name = matches[1];
             test_name = matches[2];
+
+            if (details::is_okay(buffer))
+            {
+                return_status = TestStatus_OK;
+                add_to_cases();
+            }
+
+            if (details::is_failed(buffer))
+            {
+                return_status = TestStatus_FAILED;
+                add_to_cases();
+            }
+            else if (details::is_skipped(buffer))
+            {
+                return_status = TestStatus_SKIPPED;
+                add_to_cases();
+            }
 
             continue;
         }
 
-        if (in_test)
-        {
-            if (buffer == "[OK]")
-            {
-                return_status = TestStatus_OK;
-                add_to_cases();
-
-                continue;
-            }
-            else if (buffer == "[FAILED]")
-            {
-                return_status = TestStatus_FAILED;
-                add_to_cases();
-
-                continue;
-            }
-            else if (buffer == "[SKIPPED]")
-            {
-                return_status = TestStatus_SKIPPED;
-                add_to_cases();
-
-                continue;
-            }
-        }
-
-        if (in_test && std::regex_search(buffer, matches, details::RESULTS_REGEX))
+        if (std::regex_search(buffer, matches, details::RESULTS_REGEX))
         {
             number_total = std::stoi(matches[1]);
             number_ok = std::stoi(matches[2]);
             number_failed = std::stoi(matches[3]);
             number_skipped = std::stoi(matches[4]);
             total_time = std::stoi(matches[5]);
-
-            in_test = false;
 
             output.cases = cases;
             output.number_ok = number_ok;
